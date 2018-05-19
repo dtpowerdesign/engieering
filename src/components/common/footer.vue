59cb93368ae67e3a9d02f56ca64098d8b23bf497
谢东磊 <344919627@qq.com>
2018-05-19 20:48:18 +0800

    upadte:footer updated

--- src/components/common/footer.vue
+++ src/components/common/footer.vue
@@ -1,5 +1,5 @@
 <template>
-<v-footer :style="{ textAlign: 'center', backgroundColor: '#2c3e50', height: '40px', color: '#fff', marginTop: '60px', lineHeight: '10px' }">
+<v-footer :style="{ textAlign: 'center' }">
   工程训练中心 ©2016 Created by Dre@mTech Studio
 </v-footer>
 </template>
@@ -7,5 +7,14 @@
 export default {}
 </script>
 <style>
+.ant-layout-footer {
+  background-color: #2c3e50;
+  height: 40px;
+  color: #fff;
+  margin-top: 60px;
+  line-height: 10px;
+  width: 80%;
+  margin: 0 auto;
+  min-width: 1366px;
+}
 </style>
-
