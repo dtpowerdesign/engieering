88dee35a189096cdd1850c79430728dd6a16e34b
谢东磊 <344919627@qq.com>
2018-05-19 14:29:52 +0800

    add:header

---
+++ src/components/Index.vue
@@ -0,0 +1,32 @@
+<template>
+    <div id="components-layout-demo-top">
+        <v-layout class="layout">
+            <v-header>
+                <v-menu theme="dark" mode="horizontal" :data="menuData1" :style="{lineHeight: '64px'}"></v-menu>
+            </v-header>
+        </v-layout>
+    </div>
+</template>
+
+<style>
+</style>
+<script>
+export default {
+  data () {
+    return {
+      menuData1: [
+        {
+          name: 'nav 1'
+        },
+        {
+          name: 'nav 2',
+          selected: true
+        },
+        {
+          name: 'nav 3'
+        }
+      ]
+    }
+  }
+}
+</script>
